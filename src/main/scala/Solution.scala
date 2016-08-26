fc00b3e1796610be301ac7ff0d72771e28ec54fd
luongbalinh <linhluongba@gmail.com>
2016-08-26 11:07:20 +0800

    test pre-push hook

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -4,6 +4,7 @@ object Solution extends App {
   MDC.put("first", "linh")
   MDC.put("last", "linh")
 
+
   val logger = LoggerFactory.getLogger(getClass)
   logger.info("msg 1")
   logger.info("msg 2")
